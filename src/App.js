95d1b11e92488821da2c2026d7846743df27d520
NikkiDev <gameworlddeveloper@gmail.com>
2021-04-20 22:32:44 +0400

    aba vcadot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import "./App.css";
+import './App.css'
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      asdasdasd asdasdasd
+      asdas
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
